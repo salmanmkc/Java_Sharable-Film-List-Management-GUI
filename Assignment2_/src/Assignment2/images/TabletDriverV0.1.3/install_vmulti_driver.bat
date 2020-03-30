d734f76f3ead9ef846825e6f0c6d7e11b87fc6d8
SalmanMKC <13schishti@gmail.com>
2020-03-30 04:05:59 +0100

    removing files

--- Assignment2_/src/Assignment2/images/TabletDriverV0.1.3/install_vmulti_driver.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo.
-echo   Are you sure you want to install vmulti driver?
-echo.
-set /P temp=Press enter to install...
-@echo on
-driver_vmulti\install_vmulti_64.bat
--- Assignment2_/src/Assignment2/images/TabletDriverV0.1.3/remove_vmulti_driver.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-echo.
-echo   Are you sure you want to remove the vmulti driver?
-echo.
-set /P temp=Press enter to remove...
-@echo on
-driver_vmulti\remove_vmulti_64.bat
